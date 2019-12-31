a76ef7fc54ec44c50a28fc9253dcb9e460e3a635
Bobw147 <bobw147@gmail.com>
2019-12-31 17:18:31 +0000

    Changed where the followers table is declared

--- app/models.py
+++ app/models.py
@@ -8,6 +8,12 @@ from hashlib import md5
 # being present when the file is checked
 # pylint: disable=E1101
 
+# Add the followers table as a many to many relationship
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+    
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -16,12 +22,6 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    # Add the followers table as a many to many relationship
-    followers = db.Table('followers',
-        db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-        db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-    )
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
