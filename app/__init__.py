ad3e28b08e509f240045e24271f9eeb03d168b2d
Bobw147 <bobw147@gmail.com>
2020-01-14 19:09:34 +0000

    Corrected typo

--- app/__init__.py
+++ app/__init__.py
@@ -58,6 +58,6 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    retrurn request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
