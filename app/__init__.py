0ea55927239bf48b270faa441f281f0f76cb0ee2
Bobw147 <bobw147@gmail.com>
2020-01-14 19:09:34 +0000

    Import and initialise flask_babel

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 from flask_mail import Mail, message_policy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -23,6 +24,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     #setup email handler for notification of system errors
