d177e89cb921afa4e820afa1b3d837fe870cf825
Bobw147 <bobw147@gmail.com>
2020-01-14 19:09:34 +0000

    Provide a get_locale() function

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -55,4 +55,9 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')       
+
+@babel.localeselector
+def get_locale():
+    retrurn request.accept_languages.best_match(app.config['LANGUAGES'])
+
 from app import routes, models, errors
