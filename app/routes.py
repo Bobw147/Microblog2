bcd9f708ef6183f66e5bd08ac0fa4feb934baa61
Bobw147 <bobw147@gmail.com>
2020-01-15 00:57:04 +0000

    Added the translation request to the server side

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
-from flask import render_template, flash, redirect, request, url_for, g
+from flask import render_template, flash, redirect, request, url_for, g, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_babel import _, get_locale
 from app import app, db
 from app.email import send_password_reset_email
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
+from app.translate import translate
 from werkzeug.urls import url_parse
 from datetime import datetime
 from guess_language import guess_language
@@ -187,3 +188,10 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
